7a92384ae87280d590c59688541f85cb2c0c5349
F3lip3007 <gomesfelipeg123@gmail.com>
2025-05-05 16:48:53 -0300

    oq o site precisa ser atualizado no logim

--- public/index.php
+++ public/index.php
@@ -1,11 +1,60 @@
+<!-- 
+
+<?php
+session_start();
+if (isset($_SESSION['logado'])){
+    header("Location:menuprincipal.php");
+
+// VIRAR FUNÇAO 
+}
+?> -->
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-</head>
+<head>
 <body>
-    dadwfsfsfsfs
+    <h2>Já sou cliente/Registrar<h2>
+
+<!-- 
+    se e possivel colocar uma funçao no html como no ex abaixo -->
+    <form action="<?php salvarCliente($conexao,$nome,$cpf,$endereco);?>" method="post">
+
+        <div class="login">
+
+        <h3>E-mail:</h3> <br>
+        <input type = "text"name="email"> <br><br>
+
+        <h3>Senha: </h3> <br>
+        <input type = "password"name="senha"> <br><br>
+
+        <input type="submit" value="Entrar" ><br>
+
+
+        <php>
+
+    </form>
+ <!-- criar um cadastra usuario -->
+    <a href="cadastra usuario">criar nova conta </a>
+    <!-- <form action="../controle/usuariohash.php" method="post">
+        <h1>Novo usuário</h1>
+    <h3>E-mail:</h3>
+    <input type="text" name="email" >
+
+    <h3>Senha:</h3>
+    <input type="text"  name="senha">
+    <br><br>
+
+    <input type="submit"    value="cadrastar "  class="btn btn-primary">
+    </form>
+ -->
+
+
 </body>
+</head>
 </html>
