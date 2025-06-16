4aae6184e5434e809b457b2bd4ae5823095ab986
Marinelli28 <marcosdanielmarinelli01@gmail.com>
2025-06-16 16:13:02 -0300

    produto
    os produtos devem ser feitos

--- controle/func.php
+++ controle/func.php
@@ -1,15 +1,15 @@
 <?php
 
 
-function salvarProduto($conexao,$produto,$tamanho,$valor,$estoque,$desconto,$descricao,$avaliacao,$comentatio,$tipo) {
-    $sql = "INSERT INTO td_produto (produto,tamanho,valor,estoque,desconto,descricao,avaliacao,comentatio,tipo)
+function salvarProduto($conexao,$produto,$tamanho,$valor,$estoque,$desconto,$descricao,$avaliacao,$comentatirio,$tipo) {
+    $sql = "INSERT INTO td_produto (produto,tamanho,valor,estoque,desconto,descricao,avaliacao,comentatirio,tipo)
     
     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
 
     $comando = mysqli_prepare($conexao, $sql);
     
-    mysqli_stmt_bind_param($comando, 'ssdddsdss', $produto, $tamanho,$valor,$estoque,$desconto,$descricao,$avaliacao,$comentatio,$tipo);
+    mysqli_stmt_bind_param($comando, 'ssdddsdss', $produto, $tamanho,$valor,$estoque,$desconto,$descricao,$avaliacao,$comentatirio,$tipo);
     
     $funcionou = mysqli_stmt_execute($comando);
     
@@ -18,6 +18,31 @@ function salvarProduto($conexao,$produto,$tamanho,$valor,$estoque,$desconto,$des
 
 
 }
+
+function editarProduto($conexao,$produto,$tamanho,$valor,$estoque,$desconto,$descricao,$avaliacao,$comentatirio,$tipo) {
+    $sql = "INSERT INTO td_produto (produto,tamanho,valor,estoque,desconto,descricao,avaliacao,comentatirio,tipo)
+    
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+    $comando = mysqli_prepare($conexao, $sql);
+    
+    mysqli_stmt_bind_param($comando, 'ssdddsdss', $produto, $tamanho,$valor,$estoque,$desconto,$descricao,$avaliacao,$comentatirio,$tipo);
+
+    $funcionou = mysqli_stmt_execute($comando);
+
+    mysqli_stmt_close($comando);
+    return $funcionou;
+
+
+}
+
+function deletarProduto() {};
+
+function listarProduto() {};
+
+
+
+
 
 
 ?>
