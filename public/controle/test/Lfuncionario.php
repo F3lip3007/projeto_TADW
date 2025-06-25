5d6e66a832210e34c3bf7a5d9c5b3b56789531ac
Marinelli28 <marcosdanielmarinelli01@gmail.com>
2025-06-25 09:41:09 -0300

    listar funcionario
    funcionou

--- public/controle/test/Lfuncionario.php
+++ public/controle/test/Lfuncionario.php
@@ -2,7 +2,12 @@
 require_once "../conexao.php";
 require_once "../func.php";
 
+$idfuncionario = "1";
+$salario = "1111";
+$cpf = "111.111.111.11";
+$data_nacimento = "11/11/2000";
+
 echo"<pre>";
-print_r(listarFuncionario($conexao));
+print_r(listarFuncionario($conexao, $idfuncionario, $salario, $cpf, $data_nacimento));
 echo"</pre>";
 ?>
