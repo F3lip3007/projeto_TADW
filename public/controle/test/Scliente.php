180af0f611ceb1fc9671267b92981ec169d1534f
Felipe <gomesfelipeg123@gmail.com>
2025-06-25 08:50:34 -0300

    Salvar funcionario e cliente 100%

--- public/controle/test/Scliente.php
+++ public/controle/test/Scliente.php
@@ -3,10 +3,10 @@ require_once "../conexao.php";
 require_once "../func.php";
 
 $numero="192738213";
-$cpf="200.000.000-00";
+$cpf="210.000.000-00";
 
 
 salvarCliente($conexao,$numero,$cpf,);
         
 
-?>
+?>  
--- public/controle/test/Sfuncionario.php
+++ public/controle/test/Sfuncionario.php
@@ -6,6 +6,7 @@ $salario = "12";
 $cpf = "1";
 $data_nacimento = "2012-12-12";
 
+
 salvarFuncionario($conexao, $salario, $cpf, $data_nacimento)
 
 ?>
