2447c8439d194dffae763245a50dd7db88161c9c
Marinelli28 <marcosdanielmarinelli01@gmail.com>
2025-06-25 13:28:14 -0300

    o index
    todo o index

--- public/index.php
+++ public/index.php
@@ -18,35 +18,36 @@
     <head>
 
     <body>
-        <h2>Já sou cliente/Registrar<h2>
-
-                <form action="<?php salvarCliente($conexao, $nome, $cpf, $endereco); ?>" method="post">
-                    <div class="login">
-
-                        <h3>E-mail:</h3> <br>
-                        <input type="text" name="email"> <br><br>
-
-                        <h3>Senha: </h3> <br>
-                        <input type="password" name="senha"> <br><br>
-
-                        <input type="submit" value="Entrar"><br>
+    <h3>Salvar</h3>
+        <a href="controle/test/Scliente.php">Salvar Cliente</a><br>
+        <a href="controle/test/Sproduto.php">Salvar Produto</a><br>
+        <a href="controle/test/Sfuncionario.php">Salvar Funcionario</a><br>
+        <a href="controle/test/Susuario.php">Salvar Usuario</a><br>
+        <a href="controle/test/Svenda.php">Salvar Venda</a><br><br><br>
 
 
-                        <php>
-                </form>
-                <a href="cadastra usuario">criar nova conta </a>
-                <form action="../controle/usuariohash.php" method="post">
-                    <h1>Novo usuário</h1>
-                    <h3>E-mail:</h3>
-                    <input type="text" name="email">
+    <h3>Listar</h3>
+        <a href="controle/test/Lcliente.php">Listar Cliente</a><br>
+        <a href="controle/test/Lproduto.php">Listar Produto</a><br>
+        <a href="controle/test/Lfuncionario.php">Listar Funcionario</a><br>
+        <a href="controle/test/Lusuario.php">Listar Usuario</a><br>
+        <a href="controle/test/Lvenda.php">Listar Venda</a><br><br><br>
 
-                    <h3>Senha:</h3>
-                    <input type="text" name="senha">
-                    <br><br>
 
-                    <input type="submit" value="cadrastar " class="btn btn-primary">
-                </form>
+    <h3>Editar</h3>
+        <a href="controle/test/Ecliente.php">Editar Cliente</a><br>
+        <a href="controle/test/Eproduto.php">Editar Produto</a><br>
+        <a href="controle/test/Efuncionario.php">Editar Funcionario</a><br>
+        <a href="controle/test/Eusuario.php">Editar Usuario</a><br>
+        <a href="controle/test/Evenda.php">Editar Venda</a><br><br><br>
 
+
+    <h3>Deletar</h3>
+        <a href="controle/test/Dcliente.php">Deletar Cliente</a><br>
+        <a href="controle/test/Dproduto.php">Deletar Produto</a><br>
+        <a href="controle/test/Dfuncionario.php">Deletar Funcionario</a><br>
+        <a href="controle/test/Dusuario.php">Deletar Usuario</a><br>
+        <a href="controle/test/Dvenda.php">Deletar Venda</a><br><br><br>
 
 
     </body>
