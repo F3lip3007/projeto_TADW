9832ffdb1481b1e3e0d2719a298197b2720f8885
F3lip3007 <gomesfelipeg123@gmail.com>
2025-09-03 10:57:18 -0300

    js

--- public/controle/salvar_U.php
+++ public/controle/salvar_U.php
@@ -1,14 +1,14 @@
 <?php
-require_once "../conexao.php";
-require_once "../func.php";
 
-if (){
-}
-elseif(){
-} 
-else {
-}
+require_once "./conexao.php";
+require_once "./func.php";
 
+$email=$_POST['email'];
+$email=$_POST['senha'];
+
+
+// tem que enviar para a funcao salvar fotas
+$email=$_POST['foto'];
 
 
 
