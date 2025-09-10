48ac6093f663490266248d211bb2f7dc759ea667
F3lip3007 <gomesfelipeg123@gmail.com>
2025-09-10 09:42:59 -0300

    tes

--- public/controle/salvar_U.php
+++ public/controle/salvar_U.php
@@ -1,12 +1,11 @@
+<?php
+$numero = $_POST['numero'];
+$nome = $_POST['nome'];
+$cpf = $_POST['cpf'];
+$telefone = $_POST['telefone'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    sasa    
-</body>
-</html>
+$foto = $_POST['foto'];
+
+?>
