ea374ba72b0f3c6d73a0eff076a1aa5915be51dc
F3lip3007 <gomesfelipeg123@gmail.com>
2025-09-18 13:41:25 -0300

    informaco card

--- public/area_produto.php
+++ public/area_produto.php
@@ -18,7 +18,19 @@ require_once './controle/func.php';
 
 
 
+
+
 <?php
+
+
+
+
+
+
+
+
+
+
 $produtos=listarProduto($conexao);
 
 
@@ -29,19 +41,19 @@ foreach ($produtos as $produto) {
     echo "<p>Tamanho: ".$produto['tamanho']."</p>";
     echo "<p>Valor: R$ ".$produto['valor']."</p>";
     echo "<p>Estoque: ".$produto['estoque']."</p>";
-// o descoto so deve aparecer se tiver desconto
+    // o descoto so deve aparecer se tiver desconto
     if ($produto['desconto'] > 0) {
         echo "<p>Desconto: ".$produto['desconto']."%</p>";
     }
-
-    echo "<p>Descrição: ".$produto['descricao']."</p>";
+    
+    echo "<p>Descrição: ".$produto['descricao']."</p>"; 
     echo "<p>Avaliação: ".$produto['avaliacao']." estrelas</p>";
     echo "<p>Comentário: ".$produto['comentario']."</p>";
+    echo'<a href="">aaa</a>';
     echo "<hr>";
 }
 
 // quando clicar ir para a paguina de um produto
-echo'<a href="">aaa</a>';
 
 
 
