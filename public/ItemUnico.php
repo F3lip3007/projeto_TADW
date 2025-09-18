470b0b39bc66936412c860a54f6814a6b949fb5f
F3lip3007 <gomesfelipeg123@gmail.com>
2025-09-18 14:39:14 -0300

    coisa

---
+++ public/ItemUnico.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once './controle/conexao.php';
+require_once './controle/func.php';
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<input type="number" id="estoque" class="input" >
+
+<?php
+
+
+// $id_produto=$_GET['id_produto'];
+
+
+$id_produto=1; // so para teste
+
+
+$produto=pesquisarProdutoPorId($conexao,$id_produto);
+echo "<h2>".$produto['produto']."</h2>";
+echo "<p>Tamanho: ".$produto['tamanho']."</p>";
+echo "<p>Valor: R$ ".$produto['valor']."</p>";
+echo "<p>Estoque: ".$produto['estoque']."</p>";
+// o descoto so deve aparecer se tiver desconto
+if ($produto['desconto'] > 0) {
+    echo "<p>Desconto: ".$produto['desconto']."%</p>";
+}
+
+echo "<p>Descrição: ".$produto['descricao']."</p>"; 
+echo "<p>Avaliação: ".$produto['avaliacao']." estrelas</p>";
+echo "<p>Comentário: ".$produto['comentario']."</p>";
+echo"<input type='number' id='estoque' class='input' value='1' min='1' min='1' max='<?php echo $produto['estoque']; ?>'>";
+echo'<a href="">coprar</a>';
+echo "<hr>";    
+
+
+
+
+        ?>
+</body>
+</html> 
