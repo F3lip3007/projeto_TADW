0803f9c319cff17f3d6c07f971f4d432918bbc2e
F3lip3007 <gomesfelipeg123@gmail.com>
2025-11-06 16:37:40 -0300

    BANCO CU

--- public/controle/venda.php
+++ public/controle/venda.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
-$id_u=$_SESSION['id_usuario'];
+// $id_u=$_SESSION['id_usuario'];
 $tb_id_cliente=$_SESSION['id_cliente'];
+// echo$id_u;
+
 
 require_once "./conexao.php";
 require_once "./func.php";
@@ -25,47 +27,23 @@ $valor_venda = $valor_unitario * $quantidade;
 
 $cupom = 0;
 
-$id_venda=salvarVenda($conexao, $cupom, $valor_venda, $tb_id_cliente)
+$id_venda=salvarVenda($conexao, $cupom, $valor_venda, $tb_id_cliente);
+echo$id_venda;
 
-salvarItemVenda($conexao, $id_venda, $id_produto, $quantidade)
 // mecher depois
 
 
-foreach($produt as $iq){
-    
-    // echo "<pre>";
-    // print_r($iq);   
-    // echo "</pre>";
-    
-    // se existir um id
-    if (isset($iq[0])) {
-        foreach($valor as $v){
-            if($iq[0] == $v['idproduto']) { // ta funcionamdo nao sei o pq na mecher //
-                $divida = $iq[1] * $v['preco_venda'];
-                    $valor_total= $valor_total + $divida;
-            };
-            }
 
-        }else{};        
-    
-};
-echo "<h1>$nome</h1>";
-echo "<h2>$data</h2>";  
-echo '$';
-echo $valor_total;
 
     
 $id_venda=salvarVenda($conexao, $cupom, $valor_venda, $tb_id_cliente);
 
 
-foreach ($produt as $p) {
-    if(isset($p[0],$p[1]))
-        salvarItemVenda($conexao, $id_venda, $p[0], $p[1]);
-}
+salvarItemVenda($conexao, $id_venda, $id_produto, $quantidade);
 
 ?>  
 
-?>
+
 
 
 
